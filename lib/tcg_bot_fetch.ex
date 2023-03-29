9b46a877d3a19c7508ce80cafef4bd24f633dbc8
Lucas Yugo Macedo <lucas.y.macedo@gmail.com>
2023-03-29 18:31:52 -0300

    Removed redundant code

--- lib/tcg_bot_fetch.ex
+++ lib/tcg_bot_fetch.ex
@@ -1,6 +1,5 @@
 defmodule TcgBotFetch do
   def fetch(name) do
-    # IO.inspect(URI.parse("https://api.scryfall.com/cards/named?format=image&fuzzy="<>name))
     URI.parse("https://api.scryfall.com/cards/named?format=image")
     |> URI.append_query(URI.encode_query(%{fuzzy: name}))
     |> URI.to_string()
@@ -9,12 +8,9 @@ defmodule TcgBotFetch do
 
   defp check_url(url) do
     response = HTTPoison.get!(url)
-    IO.inspect(response)
 
     if response.status_code == 404 do
-      jason_map = Jason.decode!(response.body)
-      IO.inspect(jason_map)
-      %{"details" => details} = jason_map
+      %{"details" => details} = Jason.decode!(response.body)
       details
     else
       url
