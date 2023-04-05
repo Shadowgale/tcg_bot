80dbcdd459788a51d2231c8037f1183902172946
Lucas Yugo Macedo <lucas.y.macedo@gmail.com>
2023-04-05 18:34:53 -0300

    formatting

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,7 @@ import Config
 config(
   :nostrum,
   # The token of your bot as a string
-    [{:token, ""},
-    {:gateway_intents, :all}]
-  )
+  [{:token, ""}, {:gateway_intents, :all}]
+)
 
 if File.exists?("config/secret.exs"), do: import_config("secret.exs")
