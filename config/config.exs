38409401a24303a77de691700d67de118f31d8bc
Lucas Yugo Macedo <lucas.y.macedo@gmail.com>
2023-04-06 19:09:16 -0300

    hid bot string

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,9 @@
 import Config
 
-config(
-  :nostrum,
-  # The token of your bot as a string
-  [{:token, ""}, {:gateway_intents, :all}]
-)
+config :nostrum,
+  token: "",
+  gateway_intents: [
+    :all
+  ]
 
 if File.exists?("config/secret.exs"), do: import_config("secret.exs")
